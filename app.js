6980203f64f727f13132870c7535fff30f798b8f
Devayo <gbolagadewinner@gmail.com>
2023-09-07 13:47:50 -0700

    Error Handler added

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ app.use("/orders", orderRoutes);
 //error handling middleware
 app.use((req, res, next) => {
   const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 module.exports = app;
