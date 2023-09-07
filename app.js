061de0482128789419ca161909c49183efa25607
Devayo <gbolagadewinner@gmail.com>
2023-09-07 14:40:01 -0700

    Cors errors handling

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//Handle cors error
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //Import product routes here
 const productRoutes = require("./api/routes/product");
